2bebda7bf3a91e7449ddfb7374ddb80dd48ffbb2
sarah-workman <swork623@gmail.com>
2022-11-12 11:54:57 -0600

    add cloud functions

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+admin.initializeApp();
